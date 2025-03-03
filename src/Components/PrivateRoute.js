c884f830c6c71b2c57d9cd7f9689d506d52aab56
Your Name <your.email@example.com>
2025-03-03 09:10:51 -0500

    Remove redundant JS version of PrivateRoute

--- src/Components/PrivateRoute.js
+++
@@ -1,5 +0,0 @@
-// Re-export PrivateRoute from the correct location
-// This file exists to support legacy imports in the codebase
-import PrivateRoute from '../Components/auth/PrivateRoute.tsx';
-
-export default PrivateRoute; 
